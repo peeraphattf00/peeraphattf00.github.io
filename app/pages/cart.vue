c38390721cd3798027aa554cea0922cca3527f71
kxexvxixn <wanttofast@gmail.com>
2026-08-09 15:41:06 +0700

    deploy v1

--- app/pages/cart.vue
+++ app/pages/cart.vue
@@ -1,3 +1,55 @@
 <template>
   <h1>Cart pages</h1>
+  <div v-for="item in myCart">
+    <div>
+      ID: {{ item.id }}
+    </div>
+    <div>
+      Name: {{ item.name }}
+    </div>
+    <button @click="handleClickDelete(item.id)">ลบ</button>
+    <hr />
+  </div>
 </template>
+
+<script setup>
+import dataMock from "~/data/mock-products.json"
+
+const myCart = ref([])
+
+const getProducts = () => {
+  const ls = localStorage.getItem("products")
+  return ls
+}
+
+const handleClickDelete = (productId) => {
+  console.log('id', productId)
+  const ls = getProducts()
+  const lsArray = JSON.parse(ls)
+  const result = lsArray.filter((item) => {
+    return item !== productId
+  })
+  const resultStr = JSON.stringify(result)
+  localStorage.setItem("products", resultStr)
+
+  // Update Render data
+  updateMyCart()
+}
+
+const updateMyCart = () => {
+  const productsLS = getProducts()
+  const productsArray = JSON.parse(productsLS)
+
+  const result = dataMock.products.filter((item) => {
+    // logic
+    return productsArray.includes(item.id)
+  })
+
+  myCart.value = result
+}
+
+onMounted(() => {
+  // initial data
+  updateMyCart()
+})
+</script>
--- app/pages/product.vue
+++ app/pages/product.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Product Pages</h1>
+  <h1>Product page.</h1>
+  <div v-for="item in data.products">
+    <div>ID: {{ item.id }}</div>
+    <div>Name: {{ item.name }}</div>
+    <button @click="handleClickAdd(item.id)">Add</button>
+    <hr />
+  </div>
 </template>
+
+<script setup>
+import mockProduct from "~/data/mock-products.json"
+
+const data = ref([])
+
+const handleClickAdd = (productId) => {
+  const ls = localStorage.getItem('products')
+  const lsArray = JSON.parse(ls)
+  const products = lsArray || []
+  products.push(productId)
+  const productsStr = JSON.stringify(products)
+  localStorage.setItem('products', productsStr)
+}
+
+onMounted(() => {
+  data.value = mockProduct
+  console.log('assign done !')
+})
+</script>
